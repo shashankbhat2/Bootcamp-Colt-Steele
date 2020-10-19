5eb3f88e27c5072a1fe5961b2c222774d4799daa
shashank bhat <shashank.bhat2525@gmail.com>
2020-10-19 20:57:52 +0530

    added js file

--- Javascript/index.js
+++ Javascript/index.js
@@ -1,1 +1,28 @@
-setTimeout(alert("hello"),1)
+const logger = require('nodelogeasy')
+//var & const
+const pi = 3.14
+var a = 2 + pi;
+var a = 3 + pi;
+// String Methods - slice(), indexOf()
+var name = "haha that is so funny"
+logger.log(name.slice(10), "hello")
+logger.log(name.indexOf("s"))
+logger.log(name.slice(-1))
+logger.log(name.replace('haha', 'lol'))
+logger.log(name)
+
+// template literal - `${template.string}`
+var product = "Iphone 12"
+var price = "$" + 799;
+logger.log(`${product} is ${price}`)
+
+// Math Methods
+logger.log('Math Methods')
+logger.log(Math.ceil(Math.random() * 10)+ 100)
+logger.log(Math.pow(0,0))
+
+//Console 
+logger.log('hello')
+logger.errorlog('error!')
+logger.warnlog('warn')
+//Conditionals
